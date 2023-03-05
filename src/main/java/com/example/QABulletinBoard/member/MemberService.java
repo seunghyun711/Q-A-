e52c81fb931e32ff44f5ce8c0599cd37ebaf2bf0
seunghyun711 <seunghyun711@gmail.com>
2023-03-06 01:16:31 +0900

    MemberService에 회원조회 검증 코드 추가

--- src/main/java/com/example/QABulletinBoard/member/MemberService.java
+++ src/main/java/com/example/QABulletinBoard/member/MemberService.java
@@ -26,10 +26,27 @@ public class MemberService {
 
     }
 
-    private void verifyExistsEmail(String email) {
+    private void verifyExistsEmail(String email) { // 중복 회원 검증
         Optional<Member> member = memberRepository.findByEmail(email);
         if (member.isPresent()) { // 이미 존재하는 회원인 경우 예외 발생
-            throw new BusinessLogicException(ExceptionCode.MEMBER_EXIXTS);
+            throw new BusinessLogicException(ExceptionCode.MEMBER_EXISTS);
         }
     }
+
+    // 회원 조회 검증(해당 id의 회원이 존재하는지 확인)
+    public Member findVerifiedMember(long memberId) {
+        Optional<Member> optionalMember =
+                memberRepository.findById(memberId);
+        Member findMember =
+                optionalMember.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findMember;
+    }
+
+//    public void checkMemberRole(String email) { // 회원 종류 검증
+//        Optional<Member> member = memberRepository.findByEmail(email);
+//        if (member.get().getEmail().equals("admin@gmail.com")) { // 관리자 계정은 질문 등록 불가
+//            throw new BusinessLogicException(ExceptionCode.CANNOT_REGISTRATION_QUESTION);
+//        }
+//    }
 }
