befa01dc6bdb36e3e91134e1b7f190b9fa03e1fd
seunghyun711 <seunghyun711@gmail.com>
2023-03-06 01:19:57 +0900

    OrderPostDto, OrderRepository 생성

---
+++ src/main/java/com/example/QABulletinBoard/question/QuestionRepository.java
@@ -0,0 +1,7 @@
+package com.example.QABulletinBoard.question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+}
