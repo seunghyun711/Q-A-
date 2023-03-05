7262a39992b30f705f65433a74c4d54a0635b9ee
seunghyun711 <seunghyun711@gmail.com>
2023-03-05 12:24:07 +0900

    init

---
+++ src/main/java/com/example/QABulletinBoard/QaBulletinBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.QABulletinBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaBulletinBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaBulletinBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QABulletinBoard/QaBulletinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QABulletinBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaBulletinBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
