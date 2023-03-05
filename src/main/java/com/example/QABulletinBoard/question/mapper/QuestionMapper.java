3e13d70fbdfaae494199f90ad1a76dcd76cbf79c
seunghyun711 <seunghyun711@gmail.com>
2023-03-06 01:19:12 +0900

    orderPostDtoToOrder mapper 수정

--- src/main/java/com/example/QABulletinBoard/question/mapper/QuestionMapper.java
+++ src/main/java/com/example/QABulletinBoard/question/mapper/QuestionMapper.java
@@ -1,5 +1,6 @@
 package com.example.QABulletinBoard.question.mapper;
 
+import com.example.QABulletinBoard.member.Member;
 import com.example.QABulletinBoard.question.Question;
 import com.example.QABulletinBoard.question.QuestionPostDto;
 import org.mapstruct.Mapper;
@@ -7,5 +8,14 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
     // QuestionPostDto -> Question
-    Question questionPostDtoToQuestion(QuestionPostDto questionPostDto);
+    default Question questionPostDtoToQuestion(QuestionPostDto questionPostDto){ // 자동 매핑하면 Member의 memberId를 가져올 수 없기 때문에 직접 매핑한다.
+        Question question = new Question();
+        Member member = new Member();
+        member.setMemberId(questionPostDto.getMemberId());
+        question.setTitle(questionPostDto.getTitle());
+        question.setContent(questionPostDto.getContent());
+
+        question.setMember(member);
+        return question;
+    }
 }
