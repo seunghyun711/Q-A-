1a9a8193df74dd6a2220896751c531954115c0c4
seunghyun711 <seunghyun711@gmail.com>
2023-04-08 22:37:34 +0900

    MemberDetailsService

--- src/main/java/com/example/QABulletinBoard/auth/userdetails/MemberDetailsService.java
+++ src/main/java/com/example/QABulletinBoard/auth/userdetails/MemberDetailsService.java
@@ -1,2 +1,77 @@
-package com.example.QABulletinBoard.auth.userdetails;public class MemberDetailsService {
+package com.example.QABulletinBoard.auth.userdetails;
+
+import com.example.QABulletinBoard.auth.utils.CustomAuthorityUtils;
+import com.example.QABulletinBoard.exception.BusinessLogicException;
+import com.example.QABulletinBoard.exception.ExceptionCode;
+import com.example.QABulletinBoard.member.Member;
+import com.example.QABulletinBoard.member.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Optional;
+
+/*
+ Custom UserDetails
+ User Role을 DB에서 조회하고, CustomAuthorityUtils로 스프링 시큐리티에게 Role 정보 제공
+ */
+@Component
+public class MemberDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+    private final CustomAuthorityUtils authorityUtils;
+
+    public MemberDetailsService(MemberRepository memberRepository, CustomAuthorityUtils authorityUtils) {
+        this.memberRepository = memberRepository;
+        this.authorityUtils = authorityUtils;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Member> optionalMember = memberRepository.findByEmail(username);
+        Member findMember = optionalMember.orElseThrow(() -> new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+
+        return new CustomDetails(findMember);
+    }
+
+    private final class CustomDetails extends Member implements UserDetails{
+        public CustomDetails(Member member) {
+            setMemberId(member.getMemberId());
+            setEmail(member.getEmail());
+            setPassword(member.getPassword());
+            setRole(member.getRole());
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return authorityUtils.createAuthorities(this.getRoles());
+        }
+
+        @Override
+        public String getUsername() {
+            return getEmail();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
 }
