0edee7ec8bb47133ad1d2c9ee29995022e27bc30
seunghyun711 <seunghyun711@gmail.com>
2023-04-09 16:28:20 +0900

    JwtAuthenticationFilter 추가

--- src/main/java/com/example/QABulletinBoard/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/QABulletinBoard/auth/filter/JwtAuthenticationFilter.java
@@ -1,2 +1,89 @@
-package com.example.QABulletinBoard.auth.filter;public class JwtAuthenticationFilter {
+package com.example.QABulletinBoard.auth.filter;
+
+import com.example.QABulletinBoard.auth.dto.LoginDto;
+import com.example.QABulletinBoard.auth.jwt.JwtTokenizer;
+import com.example.QABulletinBoard.member.Member;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+// 로그인 인증 요청 처리하는 Custom Security Filter
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenizer jwtTokenizer;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtTokenizer jwtTokenizer) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
+    // 메서드 내부에서 인증을 시도하는 로직 구현
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        ObjectMapper objectMapper = new ObjectMapper(); // 클라이언트에서 전송한 username, password를 DTO 클래스로 역직렬화 하기 위해 ObjectMApper 생성
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class); // ServletInputStream을 LoginDto클래스의 객체로 역직렬화
+
+        // Username, Password 포함한 UsernamePasswordAuthenticationToken 생성
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        // UsernamePasswordAuthenticationToken을 AuthenticationManager에 전달하여 인증 처리 위임
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    // 클라이언트의 인증 정보를 이용해 인증에 성공할 경우 호출되는 메서드
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) {
+        // Member 객체를 얻는다. AuthenticationManager내에서 인증이 성공하면 인증된 Authentication객체가 생성되어 principal 필드에 Member 객체가 할당된다.
+        Member member = (Member) authResult.getPrincipal();
+
+        String accessToken = delegateAccessToken(member); // Access Token 생성
+        String refreshToken = delegateRefreshToken(member); // Refresh Token 생성
+
+        // 클라이언트에서 백엔드 애플리케이션에 요청을 보낼 때마다 request Header에 추가하여 클라이언트 측의 자격증명에 사용
+        response.setHeader("Authorization", "Bearer" + accessToken);
+        // Refresh Token 추가
+        response.setHeader("Refresh", refreshToken); // 4-5
+    }
+
+    // Access Token 생성 메서드
+    private String delegateAccessToken(Member member) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("memberId", member.getMemberId());
+        claims.put("username", member.getEmail());
+        claims.put("roles", member.getRoles());
+
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    // RefreshToken 생성 메서드
+    private String delegateRefreshToken(Member member) {
+        String subject = member.getEmail();
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodeBase64SecretKey(jwtTokenizer.getSecretKey());
+
+        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
 }
